23bc3254e4c5358f9c100bb1886a655102b6e7e4
Kairgeldin Dmitry <dimgxj@gmail.com>
2024-12-13 19:05:16 +0500

    feat: readme

--- README.md
+++ README.md
@@ -1,10 +1,13 @@
 # StudyBuddy
+
 StudyBuddy is a platform designed to connect people with shared interests in learning. The goal of the project is to help users find study partners or mentors for various topics and skills, making the learning process more collaborative and engaging.
 
 # Devs
-Kairgeldin Dmitriy & Khanapin Ernar
+
+Kairgeldin Dmitriy & Khanapin Ernar & Sabina Abdikhalikova
 
 # Features:
+
 User Registration & Authentication: Users can create an account and log in.<br>
 User Profiles: Users can specify what topics they are interested in learning.<br>
 Search for Study Partners: Find people who share similar learning interests.<br>
@@ -13,6 +16,7 @@ Create Events: Users can organize study sessions or group learning events.<br>
 Ratings & Reviews: Leave feedback on study partners to help others make informed choices.<br>
 
 # Tools and Resources:
+
 Backend: Golang<br>
 Frontend: React<br>
 DB: Postgresql<br>
@@ -22,4 +26,5 @@ https://github.com/swaggo/swag<br>
 https://github.com/zhashkevych/go-clean-architecture
 
 # How to setup?
-. . . . . . . . . 
+
+. . . . . . . . .
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -4,18 +4,143 @@ import { useEffect, useState } from 'react'
 
 export default function Home() {
   const [data, setData] = useState<any>(null)
-  useEffect(() => {
-    const getData = async () => {
-      const res: any = await axios.get('http://localhost:8080/auth/user/24')
-      console.log(res)
-      setData(res.data)
-    }
-    getData()
-  }, [])
+  const [name, setName] = useState('')
+  const [userName, setUserName] = useState('')
+  const [password, setPassword] = useState('')
+  const [message, setMessage] = useState('')
+
+  const [id, setId] = useState(0)
+  // useEffect(() => {
+  //   const getData = async () => {
+  //     const res: any = await axios.get('http://localhost:8080/auth/user/24')
+  //     setData(res.data)
+  //   }
+  //   getData()
+  // }, [])
+
+  const handleCreateUser = async () => {
+    const res = await axios.post('http://localhost:8080/auth/create', {
+      name,
+      username: userName,
+      passwordhash: password,
+    })
+    setMessage('User succesfully created')
+  }
+
+  const handleGetUser = async (id: number) => {
+    const res = await axios.get(`http://localhost:8080/auth/user/${id}`)
+    setData(res.data)
+  }
+
+  const handleDeleteUser = async (id: number) => {
+    await axios.delete(`http://localhost:8080/auth/user/${id}`)
+  }
+
+  const handleUpdateUser = async (id: number) => {
+    await axios.put(`http://localhost:8080/auth/user/${id}`, {
+      name,
+      username: userName,
+      passwordhash: password,
+    })
+  }
+
   return (
-    <div>
-      <h1 style={{ color: 'white' }}>{data?.user?.name}</h1>
-      <p></p>
+    <div className='flex flex-column justify-content-center align-items-center gap-3'>
+      <div>
+        <h1>Create user</h1>
+        <input
+          type='text'
+          placeholder='Enter Name'
+          onChange={(e) => setName(e.target.value)}
+        />
+        <input
+          type='text'
+          placeholder='Enter User Name'
+          onChange={(e) => setUserName(e.target.value)}
+        />
+        <input
+          type='text'
+          placeholder='Enter Password'
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button
+          onClick={handleCreateUser}
+          className='pl-5 pr-5 bg-slate-400 rounded-full mt-5'
+        >
+          Create
+        </button>
+        {message && <p>{'User succesfully created'}</p>}
+      </div>
+
+      <hr />
+
+      <div>
+        <h1>Update user</h1>
+
+        <input
+          type='text'
+          placeholder='Enter Name'
+          onChange={(e) => setName(e.target.value)}
+        />
+        <input
+          type='text'
+          placeholder='Enter User Name'
+          onChange={(e) => setUserName(e.target.value)}
+        />
+        <input
+          type='text'
+          placeholder='Enter Password'
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button
+          onClick={() => handleUpdateUser(id)}
+          className='pl-5 pr-5 bg-slate-400 rounded-full mt-5'
+        >
+          Update
+        </button>
+        {message && <p>{'User succesfully created'}</p>}
+      </div>
+
+      <hr />
+
+      <div>
+        <h1>Get user by id</h1>
+        <input
+          type='text'
+          placeholder='ID'
+          onChange={(e) => setId(Number(e.target.value))}
+        />
+        <button
+          onClick={() => handleGetUser(id)}
+          className='pl-5 pr-5 bg-slate-400 rounded-full mt-5'
+        >
+          Get
+        </button>
+        {data !== null && (
+          <div>
+            <h1>{data.name}</h1>
+            <h1>{data.username}</h1>
+            <h1>{data.passwordhash}</h1>
+          </div>
+        )}
+      </div>
+
+      <hr />
+
+      <div>
+        <h1>Delete user</h1>
+        <input
+          type='text'
+          placeholder='ID'
+          onChange={(e) => setId(Number(e.target.value))}
+        />
+        <button
+          onClick={() => handleDeleteUser(id)}
+          className='pl-5 pr-5 bg-slate-400 rounded-full mt-5'
+        >
+          Delete
+        </button>
+      </div>
     </div>
   )
 }
