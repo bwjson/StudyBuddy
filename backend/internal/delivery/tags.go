1020debe5f0c0de19d95d47556c3265f38cb8414
likoscp <Abdikhalikova_sabina@protonmail.com>
2024-12-24 13:39:32 +0500

    fix sort

--- backend/internal/delivery/tags.go
+++ backend/internal/delivery/tags.go
@@ -100,6 +100,13 @@ func (h *Handler) getAllTags(c *gin.Context) {
 func (h *Handler) getSortOrder(c *gin.Context) (string, error) {
 	sortBy := c.DefaultQuery("sort_by", "id")
 	sortOrder := c.DefaultQuery("sort_order", "asc")
+
+	
+	
+	if sortBy != "id" && sortBy != "name" {
+		return "", fmt.Errorf("invalid sort_by parameter")
+	}
+
 	if sortOrder != "asc" && sortOrder != "desc" {
 		return "", fmt.Errorf("invalid sort_order parameter")
 	}
@@ -117,7 +124,7 @@ func (h *Handler) getPagination(c *gin.Context) (int, int, error) {
 	page := c.DefaultQuery("page", "1")
 	limit := 10
 	pageNum, err := strconv.Atoi(page)
-	if err != nil || pageNum < 1 {
+	if err != nil || pageNum < 1  {
 		return 0, 0, fmt.Errorf("invalid page number")
 	}
 	offset := (pageNum - 1) * limit
